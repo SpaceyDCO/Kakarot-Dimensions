2c3b8d6b399433bdb8d556ce38214107a749a56f
SpaceyDCO <spaidytroll.gamer@gmail.com>
2025-10-08 08:11:17 -0400

    Made purchaseUpgrade "transactional" and added documentation

--- src/main/java/com/kakarot/services/UpgradeService.java
+++ src/main/java/com/kakarot/services/UpgradeService.java
@@ -1,11 +1,15 @@
 package com.kakarot.services;
 
 import com.kakarot.Main;
+import com.kakarot.actions.Action;
+import com.kakarot.actions.ActionFactory;
 import com.kakarot.data.PurchaseResult;
 import com.kakarot.data.Upgrade;
 import com.kakarot.managers.UpgradeManager;
+import org.bukkit.configuration.ConfigurationSection;
 import org.bukkit.entity.Player;
 
+import java.util.Map;
 import java.util.Set;
 import java.util.concurrent.CompletableFuture;
 
@@ -17,10 +21,25 @@ public class UpgradeService { //OPTIMIZE TO CACHE LATER
         this.upgradeManager = plugin.getUpgradeManager();
     }
 
+    /**
+     * Attempts to purchase an upgrade
+     * This operation is transactional
+     * @param player The player purchasing the upgrade
+     * @param upgradeID The ID of the upgrade to purchase
+     * @return A CompletableFuture with a PurchaseResult enum
+     */
     public CompletableFuture<PurchaseResult> purchaseUpgrade(Player player, String upgradeID) {
         Upgrade upgrade = this.upgradeManager.getUpgrade(upgradeID)
                 .orElse(null);
         if(upgrade == null) return CompletableFuture.completedFuture(PurchaseResult.INVALID_UPGRADE);
+        for(Map.Entry<String, Object> entry : upgrade.getActions().entrySet()) {
+            if(entry.getValue() instanceof ConfigurationSection) {
+                Action action = ActionFactory.createAction(entry.getKey(), (ConfigurationSection) entry.getValue());
+                if(action != null && !action.isValid()) {
+                    return CompletableFuture.completedFuture(PurchaseResult.ACTION_INVALID);
+                }
+            }
+        }
         CompletableFuture<Integer> fragmentsFuture = plugin.getDataManager().getPlayerFragments(player.getUniqueId());
         CompletableFuture<Set<String>> unlockedUpgradesFuture = plugin.getDataManager().loadPlayerUpgrades(player.getUniqueId());
         //Add achievements later
